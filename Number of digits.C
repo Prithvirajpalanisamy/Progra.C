0a4a2e017cc92f141f47f0b9f34daf841e40a554
Prithvirajpalanisamy <35688112+Prithvirajpalanisamy@users.noreply.github.com>
2018-02-05 16:15:42 +0530

    Create Number of digits.C

---
+++ Number of digits.C
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int i, n;
+count=0;
+printf("Enter any number:");
+scanf("%d",&num);
+while(num!==0)
+{
+count++;
+num =0;
+}
+printf("total digits:%d",count);
+getch();
+return o;
+}
